20cd8e3176ea06352a21d87cca224b00648440b4
nelson <1051988660@qq.com>
2019-03-02 16:33:23 +0800

    Create Edit.vue

---
+++ src/views/Edit.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="edit-box">
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+    </div>
+</template>
+<script lang="ts">
+import { Component, Vue, Emit } from 'vue-property-decorator';
+import { Getter, Action } from 'vuex-class';
+@Component
+export default class Edit extends Vue {
+    @Action('addNote') private actionAddNote: any;
+    @Getter('getNote') private getterNote: any;
+    private mounted() {
+        this.actionAddNote('dsererer');
+    }
+}
+</script>
+<style lang="scss" scoped>
+</style>
