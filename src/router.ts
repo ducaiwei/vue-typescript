85d5a43bc011ce44861644719194f3d9cab70746
nelson <1051988660@qq.com>
2019-03-02 16:34:34 +0800

    Create router.ts

---
+++ src/router.ts
@@ -0,0 +1,30 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Index.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+      redirect: '/content',
+      children: [
+        {
+          path: '/edit',
+          name: 'edit',
+          component: () => import('./views/Edit.vue'),
+        },
+        {
+          path: '/content',
+          name: 'content',
+          component: () => import('./views/Content.vue'),
+        },
+      ],
+    },
+  ],
+});
