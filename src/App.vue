5a9496ab46da5ac932398f653b2ea7da0f4d4a2e
nelson <1051988660@qq.com>
2019-03-02 16:34:04 +0800

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  height: 100%;
+  overflow: auto;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+* {
+  margin: 0;
+  padding: 0;
+}
+html,body {
+  height: 100%;
+}
+ul, li {
+  list-style: none;
+}
+input, select, button, input:focus{
+  border: none;
+  -webkit-appearance: none;
+}
+button {
+  outline: none
+}
+</style>
