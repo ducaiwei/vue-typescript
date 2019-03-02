c701e97e6cca58c1358dd0197854ca2bac0ccea7
nelson <1051988660@qq.com>
2019-03-02 16:32:20 +0800

    Create index.ts

---
+++ src/store/index.ts
@@ -0,0 +1,11 @@
+import actions from './actions';
+import mutations from './mutations';
+import getters from './getters';
+import Vue from 'vue';
+import Vuex from 'vuex';
+Vue.use(Vuex);
+export default new Vuex.Store({
+    actions,
+    mutations,
+    getters,
+});
