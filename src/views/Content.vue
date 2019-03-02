9272ceaf4a42b7738ef1b68ffac985e131153757
nelson <1051988660@qq.com>
2019-03-02 16:33:10 +0800

    Create Content.vue

---
+++ src/views/Content.vue
@@ -0,0 +1,16 @@
+<template>
+    <vue-markdown>## Tables</vue-markdown>
+</template>
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import VueMarkdown from 'vue-markdown';
+@Component({
+    components: {
+        VueMarkdown,
+    },
+})
+export default class NoteContent extends Vue {
+}
+</script>
+<style lang="scss" scoped>
+</style>
